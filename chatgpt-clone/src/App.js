d4623582d2152cfa2d4d2a61acff8dbdb4f57943
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-29 14:12:36 -0400

    creating the chatgpt clone ui

--- chatgpt-clone/src/App.js
+++ chatgpt-clone/src/App.js
@@ -3,7 +3,15 @@
 function App() {
   return (
     <div className="app">
-      
+      <section className="side-bar">
+        <button> + chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Stacey</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+
     </div>
   );
 }
